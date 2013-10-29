230ba72958aeb152cd69c922ecae3909b3afc722
Sve-Toch <sve_toch@bk.ru>
2013-10-29 14:47:49 +0400

    все верно, но не работает(

--- src/com/example/hw2/LoadItem.java
+++ src/com/example/hw2/LoadItem.java
@@ -8,7 +8,15 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -17,7 +25,7 @@ import android.util.Log;
 
 public class LoadItem {
 	private final static String TAG = "LoadItem";
-	private final static String iUrl = "http://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png";
+	private final static String iUrl = "http://www.android.com/";
 	public static ArrayList<Item>loaditem(){
 		ArrayList<Item>	 item = new ArrayList<Item>();
 		  Log.v(TAG, " new ArrayList");
@@ -33,53 +41,55 @@ public class LoadItem {
      return item;
 	}
 	
+	
+
 	public static Bitmap getBitmapFromURL(String src) {
-	    try {
+		HttpURLConnection connection = null;
+		Bitmap myBitmap =null;
+		try {
+		//	HttpURLConnection.setFollowRedirects(false);
+			Log.v(TAG, "Starting loading image by URL: " + iUrl);
 	        URL url = new URL(src);
-	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	        connection.setDoInput(true);
-	        connection.connect();
-	        InputStream input = connection.getInputStream();
-	        Bitmap myBitmap = BitmapFactory.decodeStream(input);
-	        return myBitmap;
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return null;
-	    }
+	        connection = (HttpURLConnection) url.openConnection();
+	 
+	  	    connection.setRequestMethod("GET");
+	  	 connection.setRequestProperty("Accept", "text/plain");
+         //  connection.setDoInput(true);
+	       connection.connect();  
+	    
+	       int responseCode = connection.getResponseCode();
+	       Log.v(TAG, "connection"+responseCode);
+	        if  (responseCode==HttpURLConnection.HTTP_OK)
+	        {
+	            Log.v(TAG, "Good connection");
+	        	InputStream input = connection.getInputStream();
+	        	BufferedInputStream buf_stream = new BufferedInputStream(input, 8192);
+	        	myBitmap = BitmapFactory.decodeStream(buf_stream);
+	  	        buf_stream.close();
+	  	      connection.disconnect();
+	        	return myBitmap;
+	        }
+	        else  return null;
+	       
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		        Log.v(TAG, "ERROR");
+		        return null;
+		    }
+		finally {
+		    if (connection != null) {
+		    connection.disconnect();
+		    Log.v(TAG, "Disconnect");
+		    return null;
+		   }
 	}
 	
-	 
-	  public static Bitmap downloadImage(String iUrl) {
-	    Bitmap bitmap = null;
-	    HttpURLConnection conn = null;
-	    BufferedInputStream buf_stream = null;
-	    try {
-	      Log.v(TAG, "Starting loading image by URL: " + iUrl);
-	      conn = (HttpURLConnection) new URL(iUrl).openConnection();
-	      conn.setDoInput(true);
-	      conn.setRequestProperty("Connection", "Keep-Alive");
-	      conn.connect();
-	      buf_stream = new BufferedInputStream(conn.getInputStream(), 8192);
-	      bitmap = BitmapFactory.decodeStream(buf_stream);
-	      buf_stream.close();
-	      conn.disconnect();
-	      buf_stream = null;
-	      conn = null;
-	    } catch (MalformedURLException ex) {
-	      Log.e(TAG, "Url parsing was failed: " + iUrl);
-	    } catch (IOException ex) {
-	      Log.d(TAG, iUrl + " does not exists");
-	    } catch (OutOfMemoryError e) {
-	      Log.w(TAG, "Out of memory!!!");
-	      return null;
-	    } finally {
-	      if ( buf_stream != null )
-	        try { buf_stream.close(); } catch (IOException ex) {}
-	      if ( conn != null )
-	        conn.disconnect();
-	    }
-	    return bitmap;
-	  }
+	}	 
+	
+	
+	
+	
+	
 	
 	
 }
